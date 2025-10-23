16a4307a581c18bf3ded2ba7676affec3d2a75fb
Syakir0 <ahmadsyakirr123@gmail.com>
2025-10-23 23:21:08 +0700

    perbaiki mahasiswa controller

--- app/Http/Controllers/Api/Admin/MahasiswaController.php
+++ app/Http/Controllers/Api/Admin/MahasiswaController.php
@@ -4,24 +4,28 @@ namespace App\Http\Controllers\Api\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Mahasiswa;
+use App\Http\Resources\MahasiswaResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
-use App\Http\Resources\MahasiswaResource;
+use App\Enums\GolonganDarah;
 
 class MahasiswaController extends Controller
 {
     /**
-     * Menampilkan list data mahasiswa
+     * Tampilkan daftar mahasiswa
      */
     public function index(Request $request)
     {
-        $mahasiswas = Mahasiswa::when($request->q, function ($query) use ($request) {
-                $query->where('nama', 'like', '%' . $request->q . '%')
-                      ->orWhere('nim', 'like', '%' . $request->q . '%')
-                      ->orWhere('prodi_id', 'like', '%' . $request->q . '%');
+        $mahasiswas = Mahasiswa::with('programStudi')
+            ->when($request->q, function ($query) use ($request) {
+                $query->where('nama', 'like', "%{$request->q}%")
+                    ->orWhere('nim', 'like', "%{$request->q}%")
+                    ->orWhereHas('programStudi', function ($q) use ($request) {
+                        $q->where('nama', 'like', "%{$request->q}%");
+                    });
             })
             ->latest()
-            ->paginate(5);
+            ->paginate(10);
 
         return new MahasiswaResource(true, 'List Data Mahasiswa', $mahasiswas);
     }
@@ -32,9 +36,15 @@ class MahasiswaController extends Controller
     public function store(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'nim'      => 'required|unique:mahasiswas',
-            'nama'     => 'required',
-            'prodi_id' => 'nullable|integer',
+            'nim'             => 'required|unique:mahasiswas',
+            'nama'            => 'required|string|max:100',
+            'program_studi_id' => 'required|exists:program_studis,id',
+            'email'           => 'required|email|unique:mahasiswas',
+            'nomor_hp'        => 'required|string|max:15|unique:mahasiswas',
+            'jenis_kelamin'   => 'required|boolean',
+            'tempat_lahir'    => 'required|string|max:100',
+            'tanggal_lahir'   => 'required|date',
+            'golongan_darah'  => 'nullable|in:A,B,AB,O',
         ]);
 
         if ($validator->fails()) {
@@ -42,20 +52,29 @@ class MahasiswaController extends Controller
         }
 
         $mahasiswa = Mahasiswa::create([
-            'nama'     => $request->nama,
-            'nim'      => $request->nim,
-            'prodi_id' => $request->prodi_id,
+            'nim'             => $request->nim,
+            'nama'            => $request->nama,
+            'program_studi_id' => $request->program_studi_id,
+            'email'           => $request->email,
+            'nomor_hp'        => $request->nomor_hp,
+            'jenis_kelamin'   => $request->jenis_kelamin,
+            'tempat_lahir'    => $request->tempat_lahir,
+            'tanggal_lahir'   => $request->tanggal_lahir,
+            'golongan_darah'  => $request->golongan_darah
+                ? GolonganDarah::from($request->golongan_darah)
+                : null,
         ]);
 
         return new MahasiswaResource(true, 'Data Mahasiswa Berhasil Disimpan!', $mahasiswa);
     }
 
     /**
-     * Menampilkan detail mahasiswa
+     * Detail mahasiswa
      */
     public function show(Mahasiswa $mahasiswa)
     {
-        return new MahasiswaResource(true, 'Detail Data Mahasiswa!', $mahasiswa);
+        $mahasiswa->load('programStudi');
+        return new MahasiswaResource(true, 'Detail Data Mahasiswa', $mahasiswa);
     }
 
     /**
@@ -64,9 +83,15 @@ class MahasiswaController extends Controller
     public function update(Request $request, Mahasiswa $mahasiswa)
     {
         $validator = Validator::make($request->all(), [
-            'nim'      => 'required|unique:mahasiswas,nim,' . $mahasiswa->id,
-            'nama'     => 'required',
-            'prodi_id' => 'nullable|integer',
+            'nim'             => 'required|unique:mahasiswas,nim,' . $mahasiswa->id,
+            'nama'            => 'required|string|max:100',
+            'program_studi_id' => 'required|exists:program_studis,id',
+            'email'           => 'required|email|unique:mahasiswas,email,' . $mahasiswa->id,
+            'nomor_hp'        => 'required|string|max:15|unique:mahasiswas,nomor_hp,' . $mahasiswa->id,
+            'jenis_kelamin'   => 'required|boolean',
+            'tempat_lahir'    => 'required|string|max:100',
+            'tanggal_lahir'   => 'required|date',
+            'golongan_darah'  => 'nullable|in:A,B,AB,O',
         ]);
 
         if ($validator->fails()) {
@@ -74,16 +99,24 @@ class MahasiswaController extends Controller
         }
 
         $mahasiswa->update([
-            'nama'     => $request->nama,
-            'nim'      => $request->nim,
-            'prodi_id' => $request->prodi_id,
+            'nim'             => $request->nim,
+            'nama'            => $request->nama,
+            'program_studi_id' => $request->program_studi_id,
+            'email'           => $request->email,
+            'nomor_hp'        => $request->nomor_hp,
+            'jenis_kelamin'   => $request->jenis_kelamin,
+            'tempat_lahir'    => $request->tempat_lahir,
+            'tanggal_lahir'   => $request->tanggal_lahir,
+            'golongan_darah'  => $request->golongan_darah
+                ? GolonganDarah::from($request->golongan_darah)
+                : null,
         ]);
 
         return new MahasiswaResource(true, 'Data Mahasiswa Berhasil Diupdate!', $mahasiswa);
     }
 
     /**
-     * Hapus data mahasiswa
+     * Hapus mahasiswa
      */
     public function destroy(Mahasiswa $mahasiswa)
     {
